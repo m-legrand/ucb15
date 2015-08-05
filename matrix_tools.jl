a7bba1211333d35d36dabb43dee78eecc1c37d9b
Maxime Legrand <pro.legrand@gmail.com>
2015-08-05 11:52:46 -0700

    Introducing mat_perm

--- matrix_tools.jl
+++ matrix_tools.jl
@@ -1,15 +1,28 @@
 # Matrix tools for work on MCL with Julia
 # Berkeley 2015 - Maxime Legrand
 
+"""Matrix permutation
+Input : A, square matrix
+    perm, permutation vector (set to shuffle([1:size(A,1)]) to retrieve the former mat_shuffle
+Output : B, A with a shuffle applied to its base of representation 
+    (vertices in the case of an adjacency matrix)"""
+function mat_perm(A; perm=[])
+    n = size(A,1)
+    if length(perm) == 0
+        perm = [1:n]
+    end
+    B = [A[perm[i],perm[j]] for i=1:n, j=1:n]
+    return B
+end
+
 """Matrix shuffle
 Input : A, square matrix
 Output : B, A with a shuffle applied to its base of representation 
     (vertices in the case of an adjacency matrix)"""
 function mat_shuffle(A)
-    n = size(A,1)
+    n = siza(A,1)
     v = shuffle([1:n])
-    B = [A[v[i],v[j]] for i=1:n, j=1:n]
-    return B
+    return mat_perm(A,perm=v)
 end
 
 """SBM clusters

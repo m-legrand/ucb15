8950792967c5223a2602267903d19f32473d38f9
Maxime Legrand <pro.legrand@gmail.com>
2015-08-05 12:12:08 -0700

    Introducing vv_perm

--- matrix_tools.jl
+++ matrix_tools.jl
@@ -4,13 +4,10 @@
 """Matrix permutation
 Input : A, square matrix
     perm, permutation vector (set to shuffle([1:size(A,1)]) to retrieve the former mat_shuffle
-Output : B, A with a shuffle applied to its base of representation 
+Output : B, A with perm applied to its base of representation 
     (vertices in the case of an adjacency matrix)"""
-function mat_perm(A; perm=[])
+function mat_perm(A, perm)
     n = size(A,1)
-    if length(perm) == 0
-        perm = [1:n]
-    end
     B = [A[perm[i],perm[j]] for i=1:n, j=1:n]
     return B
 end
@@ -20,9 +17,20 @@ Input : A, square matrix
 Output : B, A with a shuffle applied to its base of representation 
     (vertices in the case of an adjacency matrix)"""
 function mat_shuffle(A)
-    n = size(A,1)
-    v = shuffle([1:n])
-    return mat_perm(A,perm=v)
+    return mat_perm(A,shuffle([1:size(A,1)]))
+end
+
+"""Vectors vector permutation
+Input : C, vectors vector
+perm, integers permutation vector
+Output : Cp, C of which elements have been applied the given permutation"""
+function vv_perm(C, perm)
+    K = size(C,1)
+    Cp = Vector[]
+    for k = 1:K
+        push!(Cp, map(x -> perm[x], C[k])
+    end
+    return Cp
 end
 
 """SBM clusters
